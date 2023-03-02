53eae2923df51cc62b3a3729f05aa3b31a93db3b
Humayra Mahboob <humayra.mahboob01@myhunter.cuny.edu>
2023-03-02 09:29:54 -0500

    finished the lab

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,95 @@
 #include <iostream>
 #include "funcs.h"
+using namespace std;
 
-// add functions here
+// Task A. Write a program that if n is
+// divisible by d, the function should return 
+// true, otherwise return false.
+
+bool isDivisibleBy(int n, int d) {
+	if (n%d != 0) {
+		return false;
+	}
+	return true;
+}
+
+// Task B. A function that returns true if n
+// is prime, otherwise return false.
+
+bool isPrime(int n) {
+	if (n == 0 || n == 1) {
+		return false;
+	}
+	for (int i=2; i<= n-1; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Task C: Add a function that returns the 
+// smallest prime greater than n.
+
+int isNextPrime(int n) {
+	int i = n+1;
+	while (!isPrime(i)) {
+		i++;
+	}
+	return i;
+}
+
+// Task D: Add a function that returns the 
+// number of prime numbers in the interval
+// [a,b].
+
+int countPrimes(int a, int b) {
+	int count = 0;
+	for (int i= a; i<=b; i++) {
+		if (isPrime(i)) {
+			count = count + 1;
+		}
+	}
+	return count; 
+}
+
+// Task E. Add a function that determines whether
+// or not its argument is a twin prime. 
+
+bool isTwinPrime(int n) {
+	if (isPrime(n) && (isPrime(n-2) || isPrime(n+2))) {
+		return true;
+	}
+	return false;
+}
+
+// Task F. Add a function that returns the smallest
+// twin prime greater than n.
+
+int nextTwinPrime(int n) {
+	int i = n+1;
+	while (!isTwinPrime(i)) {
+		i++;
+	}
+	return i;
+}
+
+// Task G. Add a function that returns the 
+// largest twin prime in the range [a,b]. If
+// no twin primes, return -1.
+
+int largestTwinPrime(int a, int b) {
+	int num = -1;
+	for (int i=a; i<=b; i++) {
+		if (isTwinPrime(i)){
+			num = i;
+		}
+	}
+	return num; 
+}
+
+			
+
+
+
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+// Author: Humayra Mahboob
+// Course: CS-135
+// Instructor: Michael Zamansky
+// Assignment: Lab 5
+
+// Testing Tasks A-G
 #include <iostream>
-
-
 #include "funcs.h"
+using namespace std;
 
-int main()
-{
+int main() {
+	cout << " 100 is divisible by 25:" << isDivisibleBy(100,25) << endl;
+	cout << " 35 is divisible by 17:" << isDivisibleBy(35,17) << endl;
+	cout << " 2 is a prime number:" << isPrime(2) << endl;
+	cout << "6 is a prime number:" << isPrime(6) << endl;
+	cout << "The next prime number after 2 is " << isNextPrime(2) << endl;
+	cout << "The next prime number after 6 is " << isNextPrime(6) << endl;
+	cout << "The number of prime numbers between 1 and 10 are " << countPrimes(1,10) << endl;
+	cout << "5 is a twin prime:" << isTwinPrime(5) << endl;
+	cout << " 2 is a twin prime: " << isTwinPrime(2) << endl;
+	cout << " The next twin prime after 2 is " << nextTwinPrime(2) << endl;
+	cout << " The next twin prime after 10 is " << nextTwinPrime(10) << endl;
+	cout << " The largest twin prime between 1 and 10 is " << largestTwinPrime(1,10) << endl;
   return 0;
 }
